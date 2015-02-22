710b9f3797c562cc614d93ec0e769803d93edfe7
Kyle Hogan <kylehogan@Kyles-Air.home>
2015-02-21 20:19:07 -0500

    added port number to uri

--- app/controllers/food_journals_controller.rb
+++ app/controllers/food_journals_controller.rb
@@ -21,7 +21,7 @@ class FoodJournalsController < ApplicationController
 
   def show
     urlOne = "https://api.nutritionix.com/v1_1/search/";
-    urlTwo = "?item_type=3&results=0%3A20&cal_min=0&cal_max=5000&fields=item_name%2Cnf_dietary_fiber%2Cbrand_name%2Cnf_calories%2Cnf_serving_size_qty%2Cnf_serving_size_unit%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein%2Cnf_serving_weight_grams%2Citem_id%2Cbrand_id&appId=bdcc47ce&appKey=e53cc81b43727bf30f6ffb0a54ab80a8"
+    urlTwo = "?item_type=3&results=0%3A20&cal_min=0&cal_max=5000&fields=item_name%2Cnf_dietary_fiber%2Cbrand_name%2Cnf_calories%2Cnf_serving_size_qty%2Cnf_serving_size_unit%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein%2Cnf_serving_weight_grams%2Citem_id%2Cbrand_id&appId=bdcc47ce&appKey=e53cc81b43727bf30f6ffb0a54ab80a8:443"
 
     food = (params[:food])
     foodsNutrix = HTTParty.get(urlOne + URI.encode(food) + urlTwo)
@@ -73,7 +73,7 @@ class FoodJournalsController < ApplicationController
     else
       @foods = FoodJournal.where(:user_id => session[:user_id], date: Date.today.beginning_of_day..Date.today.end_of_day)
       render :file => "layouts/_food", :layout => false
-      
+
     end
   end
 
